70e0366d3b31f467ad8412c84f66f86773d0ff7e
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-12 19:00:05 +0200

    test with the file that was causing trouble

--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
 
 var request = require('request')
 var static  = require('../')
-var levelup = require('levelup')
+var levelup = require('level-test')()
 var http = require('http')
 var tape = require('tape')
-
-var db = levelup('/tmp/level-static-test')
+var fs = require('fs')
+var db = levelup('level-static-test')
 var port = ~~(Math.random()*60000) + 1024
+var shasum = require('shasum')
 
 function url() {
   return 'http://localhost:'+port+'/'+[].slice.call(arguments).join('/')
@@ -21,8 +22,7 @@ tape('simple test', function (t) {
     put.end()
     put.on('end', function () {
       request(url('foo'), function (err, _, body) {
-        console.log(err, body)
-        t.equal('hello\n', body)
+        t.equal(body, 'hello\n')
         server.close()
         t.end()
       })
@@ -31,3 +31,27 @@ tape('simple test', function (t) {
 
 })
 
+tape('large file', function (t) {
+
+  var server = http.createServer(static(db)).listen(port, function () {
+
+    var d3 = '', l = 0
+    var put = request.put(url('d3.v2.js'))
+
+    fs.createReadStream(__dirname + '/fixtures/d3.v2.js', 'utf-8')
+    .on('data', function (data) {
+      d3 += data.toString()
+      l += data.length
+    })
+    .pipe(put)
+
+    put.on('end', function () {
+      request(url('d3.v2.js'), function (err, _, body) {
+        if(err) throw err
+        t.equal(body, d3)
+        server.close()
+        t.end()
+      })
+    })
+  })
+})
